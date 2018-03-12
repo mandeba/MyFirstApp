f0b0b2ed07d50cfc8fcecc7e3baf7799fdf06344
Mandus Engman <mandus.engman@gmail.com>
2018-03-12 20:05:18 +0100

    adding port for Heroku

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import express from "express";
+
 const app = express();
+const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
     res.send("Hello World!!!");
 });
 
-app.listen(3000, () => {
-    console.log("app listening on port 3000.");
+app.listen(port, () => {
+    console.log(`app listening on port: ${port}!`);
 });
