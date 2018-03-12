4c8530b1e129f84c280f16640115fb5847ef88f8
Mandus Engman <mandus.engman@gmail.com>
2018-03-12 21:09:07 +0100

    setting up basic routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const twig = require('twig');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello Netlight!!!");
+app.set("twig options", {
+    strict_variables: false 
 });
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+const great = (req, res) => {
+    let names = null;
+    if (req.query && req.query.names) {
+        names = req.query.names;
+    }
+
+    const { name = '<i>no name provided</i>' } = req.params;
+
+    res.render("index.html.twig", {
+        name,
+        names
+    });
+};
+
+// app.get('/', (req, res) => great(req, res, '<i>no name provided</i>'));
+app.get('/:name?', great);
 
 app.listen(port, () => {
     console.log(`app listening on port: ${port}!`);
