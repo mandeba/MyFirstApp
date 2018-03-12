014e091caf4337ef395ed52cd26396992eed64dc
Mandus Engman <mandus.engman@gmail.com>
2018-03-12 20:18:35 +0100

    Cleared out babel

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import express from "express";
+const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send("Hello World!!!");
+    res.send("Hello Netlight!!!");
 });
 
 app.listen(port, () => {
